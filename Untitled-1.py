7f591e2ff7b31e634e02d17e3a995f19603d5436
ZiadMohamedTaha <ziadmohamedtaha3@gmail.com>
2023-12-11 18:40:23 +0200

    my code

---
+++ Untitled-1.py
@@ -0,0 +1,23 @@
+for x in range (5):
+    num1=int (input ("enter the frist number: "))
+    opar= (input("the opration "))
+    num2=int (input ("enter the sacund number: ")) 
+    if opar=='+':
+        res=num1+num2
+        print(res)
+    elif opar=='-':
+        res=num1-num2
+        print (res)
+    elif opar=='*':
+        res=num1*num2
+        print(res)
+    elif opar=='/':
+        res=num1/num2
+        print (res)
+
+
+    
+    
+    
+
+
---
+++ Untitled1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def zizo():
+    x=15
+    y=10
+    print(x+y)
+zizo()
+print(x)
+
+
+# In[ ]:
+
+
+if 5>2:
+    print("")
+
+
+# In[ ]:
+
+
+
+
